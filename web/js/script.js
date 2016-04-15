6d3bed74e838af76ac22f0b43647befcb2866e6c
Batandwa Colani <i@batandwa.me>
2016-04-16 00:28:32 +0200

    Skeleton observer pattern.

--- web/js/script.js
+++ web/js/script.js
@@ -1,1 +1,49 @@
-alert('One step for Batandwa');
+function main() {
+  var domain = getDomain();
+  alert(domain);
+
+  Observable.addObserver(function(message){
+    console.log("First observer message:" + message)
+  });
+
+  var observer = function(message){
+    console.log("Second observer message:" + message)
+  };
+
+  Observable.addObserver(observer);
+
+  Observable.notifyObservers('test 1');
+  // Second observer message:test 1
+  // First observer message:test 1
+
+  Observable.removeObserver(observer);
+
+  Observable.notifyObservers('test 2');
+  // First observer message:test 2
+}
+
+function getDomain() {
+  var loc = window.location;
+  return loc.hostname;
+}
+
+var Observable = {
+  observers: [],
+  addObserver: function(observer) {
+    this.observers.push(observer)
+  },
+  removeObserver: function(observer) {
+    var index = this.observers.indexOf(observer)
+
+    if (~index) {
+      this.observers.splice(index, 1)
+    }
+  },
+  notifyObservers: function(message) {
+    for (var i = this.observers.length - 1; i >= 0; i--) {
+      this.observers[i](message)
+    };
+  }
+}
+
+main();
