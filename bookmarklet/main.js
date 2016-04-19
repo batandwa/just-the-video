325f63ee0d57b68bd27d644a94e64e11e8838e5a
Batandwa Colani <i@batandwa.me>
2016-04-19 02:06:09 +0200

    Reset margins and paddings.

--- bookmarklet/main.js
+++ bookmarklet/main.js
@@ -1,3 +1,3 @@
 var p = 'scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,' +
-    'width=0,height=0,left=-1000,top=-1000';
+    'width=0,height=0,left=-400,top=-400';
 open('http://just-the-video.docker.local/?v=' + encodeURIComponent(window.location.href), 'test', p);
