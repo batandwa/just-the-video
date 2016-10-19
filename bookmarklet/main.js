e4c7f943ed261ed7e40e702cae9654dc06f70020
Batandwa Colani <i@batandwa.me>
2016-10-19 22:37:01 +0200

    Updated bookmarklet target URL.

--- bookmarklet/main.js
+++ bookmarklet/main.js
@@ -1,3 +1,3 @@
 var p = 'scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,' +
     'width=0,height=0,left=-400,top=-400';
-open('http://just-the-video.docker.local/?v=' + encodeURIComponent(window.location.href + '&autoplay=1&disablekb=1'), null, p);
+open('http://justthevideo.batandwa.me/watch.html?v=' + encodeURIComponent(window.location.href + '&autoplay=1&disablekb=1'), null, p);
