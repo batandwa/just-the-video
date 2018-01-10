15dc66f230a9869d8498a8821e76a5ed6e95692b
Batandwa Colani <i@batandwa.me>
2018-01-11 00:59:24 +0200

    Removed the export keyword.

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-export REPO=batandwa/just-the-video
-export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
-export TAG="${TAG/feature\//}"
+REPO=batandwa/just-the-video
+TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+TAG="${TAG/feature\//}"
 docker run --rm -it -v `pwd`:/code batandwa/just-the-video-build:latest sh -c "cd /code && ls -alh && npm install && grunt --gruntfile=gruntfile.js setup build"
 docker build -f Dockerfile -t $REPO:$TAG .
 docker login --username $DOCKER_REGISTRY_USER --password "$DOCKER_REGISTRY_PASSWORD"
