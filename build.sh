f8af0b2271047aeac7338506330c9875d6efdf59
Batandwa Colani <i@batandwa.me>
2018-01-11 00:54:31 +0200

    Updated build script.

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-ls -alh /
-# export REPO=batandwa/just-the-video
-# export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
-# export TAG="${TAG/feature\//}"
-# docker run --rm -it -v `pwd`:/code batandwa/just-the-video-build:latest sh -c "cd /code && ls -alh && npm install && grunt --gruntfile=gruntfile.js setup build"
-# docker build -f Dockerfile -t $REPO:$TAG .
-# docker login --username $DOCKER_REGISTRY_USER --password "$DOCKER_REGISTRY_PASSWORD"
-# docker run -d -p 80:80 $REPO:$TAG
-# sleep 5
-# curl localhost:80 --head
-# docker push $REPO:$TAG
+export REPO=batandwa/just-the-video
+export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
+export TAG="${TAG/feature\//}"
+docker run --rm -it -v `pwd`:/code batandwa/just-the-video-build:latest sh -c "cd /code && ls -alh && npm install && grunt --gruntfile=gruntfile.js setup build"
+docker build -f Dockerfile -t $REPO:$TAG .
+docker login --username $DOCKER_REGISTRY_USER --password "$DOCKER_REGISTRY_PASSWORD"
+docker run -d -p 80:80 $REPO:$TAG
+sleep 5
+curl localhost:80 --head
+docker push $REPO:$TAG
