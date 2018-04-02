3561c362be66ea4b47870457094d29c8cddd7218
Batandwa Colani <i@batandwa.me>
2018-04-02 23:21:25 +0200

    Added test output.

--- build.sh
+++ build.sh
@@ -18,5 +18,6 @@ docker push $REPO:$TAG
 openssl aes-256-cbc -K $encrypted_75ea7b9aaeb2_key -iv $encrypted_75ea7b9aaeb2_iv -in id_rsa.enc -out /tmp/id_rsa -d
 chmod 0600 /tmp/id_rsa
 ssh -o StrictHostKeyChecking=no -i /tmp/id_rsa $APP_HOST_USER@$APP_HOST mkdir -p $DEST_DIR
+ssh -o StrictHostKeyChecking=no -i /tmp/id_rsa $APP_HOST_USER@$APP_HOST ls -alh /
 scp -o StrictHostKeyChecking=no -i /tmp/id_rsa docker-compose.yml $APP_HOST:$DEST_DIR
 ssh -o StrictHostKeyChecking=no -i /tmp/id_rsa $APP_HOST cd $DEST_DIR && docker-compose up -d
