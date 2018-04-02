dc67ce2f555dd2c73a01d20d04ed6d907b6c859d
Batandwa Colani <i@batandwa.me>
2018-04-02 23:08:56 +0200

    Fixed some path and authentication issues.

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ REPO=batandwa/just-the-video
 BUILD_IMAGE=just-the-video-build
 TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
 TAG="${TAG/feature\//}"
-DEST_DIR=~/projects/just_the_video
+DEST_DIR=/home/$APP_HOST_USER/projects/just_the_video
 
 docker run --rm -it -v `pwd`:/code batandwa/$BUILD_IMAGE:latest sh -c "cd /code && ls -alh && npm install && grunt --gruntfile=gruntfile.js setup build"
 docker build -f Dockerfile -t $REPO:$TAG .
@@ -17,5 +17,6 @@ docker push $REPO:$TAG
 
 openssl aes-256-cbc -K $encrypted_75ea7b9aaeb2_key -iv $encrypted_75ea7b9aaeb2_iv -in id_rsa.enc -out /tmp/id_rsa -d
 chmod 0600 /tmp/id_rsa
-ssh -o StrictHostKeyChecking=no -i /tmp/id_rsa $APP_HOST $DEST_DIR
-scp -i /tmp/id_rsa docker-compose.yml $APP_HOST:$DEST_DIR
+ssh -o StrictHostKeyChecking=no -i /tmp/id_rsa $APP_HOST_USER@$APP_HOST mkdir -p $DEST_DIR
+scp -o StrictHostKeyChecking=no -i /tmp/id_rsa docker-compose.yml $APP_HOST:$DEST_DIR
+ssh -o StrictHostKeyChecking=no -i /tmp/id_rsa $APP_HOST cd $DEST_DIR && docker-compose up -d
