de6a7e03d82c8754fe6dc96c92f6d081394c309f
Batandwa Colani <i@batandwa.me>
2018-04-06 18:43:51 +0200

    Updated CI scripts.

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ REPO=batandwa/just-the-video
 BUILD_IMAGE=just-the-video-build
 TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
 TAG="${TAG/feature\//}"
+TAG="${TAG/release\//}"
 DEST_DIR=/home/$APP_HOST_USER/projects/just_the_video
 # APP_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 
@@ -13,3 +14,5 @@ docker login --username $DOCKER_REGISTRY_USER --password "$DOCKER_REGISTRY_PASSW
 docker run -d -p 80:80 $REPO:$TAG
 sleep 5
 curl localhost:80 --head
+
+docker push $REPO:$TAG
