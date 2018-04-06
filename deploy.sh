58d40ce00d3b21acae5ed47ad0b2e9396b8679fa
Batandwa Colani <i@batandwa.me>
2018-04-06 19:39:13 +0200

    Testing push from deploy scripts.

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,8 @@ TAG="${TAG/release\//}"
 DEST_DIR=/home/$APP_HOST_USER/projects/just_the_video
 # APP_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 
+docker push $REPO:$TAG
+
 openssl aes-256-cbc -K $encrypted_75ea7b9aaeb2_key -iv $encrypted_75ea7b9aaeb2_iv -in id_rsa.enc -out /tmp/id_rsa -d
 chmod 0600 /tmp/id_rsa
 ssh -o StrictHostKeyChecking=no -i /tmp/id_rsa $APP_HOST_USER@$APP_HOST "mkdir -p $DEST_DIR"
