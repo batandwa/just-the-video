52e527223205087cbd6e03c905a9822a73e3e7dd
Batandwa Colani <i@batandwa.me>
2018-04-07 20:16:06 +0200

    Added verbosity to build.

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_
 TAG="${TAG/feature\//}"
 TAG="${TAG/release\//}"
 DEST_DIR=/home/$APP_HOST_USER/projects/just_the_video
-# APP_VERSION=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 
-docker run --rm -it -v `pwd`:/code batandwa/$BUILD_IMAGE:latest sh -c "cd /code && ls -alh && npm install && grunt --gruntfile=gruntfile.js setup build"
+docker run --rm -it -v `pwd`:/code batandwa/$BUILD_IMAGE:latest \
+        sh -c "cd /code && ls -alh && npm install && grunt -v --gruntfile=gruntfile.js setup build"
 docker build -f Dockerfile -t $REPO:$TAG .
 docker login --username $DOCKER_REGISTRY_USER --password "$DOCKER_REGISTRY_PASSWORD"
 docker run -d -p 80:80 $REPO:$TAG
