9f25d1d0aff2dc72b36c1d9ca7f47340d90991a5
Batandwa Colani <i@batandwa.me>
2018-04-08 12:12:00 +0200

    Added check for Docker and the tag before building image in Grunt.

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ BUILD_IMAGE=just-the-video-build
 TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
 TAG="${TAG/feature\//}"
 TAG="${TAG/release\//}"
-export TAG
 DEST_DIR=/home/$APP_HOST_USER/projects/just_the_video
-echo "TAG=$TAG" >> .env
+echo "TAG=$DOCKER_TAG" >> .env
 
 which docker
 docker run --rm -it -v `pwd`:/code batandwa/$BUILD_IMAGE:latest \
         sh -c "cd /code && ls -alh && npm install && grunt -v --gruntfile=gruntfile.js setup build"
+docker build -f Dockerfile -t $REPO:$TAG .
 docker login --username $DOCKER_REGISTRY_USER --password "$DOCKER_REGISTRY_PASSWORD"
 docker run -d -p 80:80 $REPO:$TAG
 sleep 5
