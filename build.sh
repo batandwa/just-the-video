440a750040d9657e39af456c7bf6572655df6be7
Batandwa Colani <i@batandwa.me>
2018-04-08 13:32:34 +0200

    Fixed image name.

--- build.sh
+++ build.sh
@@ -9,13 +9,12 @@ DEST_DIR=/home/$APP_HOST_USER/projects/just_the_video
 echo "TAG=$DOCKER_TAG" >> .env
 echo "DOCKER_IMAGE=$DOCKER_IMAGE" >> .env
 
-which docker
 docker run --rm -it -v `pwd`:/code batandwa/$BUILD_IMAGE:latest \
         sh -c "cd /code && ls -alh && npm install && grunt -v --gruntfile=gruntfile.js setup build"
 echo "Starting build of image..."
-docker build -f Dockerfile -t $REPO:$TAG .
+docker build -f Dockerfile -t $DOCKER_IMAGE:$TAG .
 docker login --username $DOCKER_REGISTRY_USER --password "$DOCKER_REGISTRY_PASSWORD"
-docker run -d -p 80:80 $REPO:$TAG
+docker run -d -p 80:80 $DOCKER_IMAGE:$TAG
 sleep 5
 curl localhost:80 --head
 
