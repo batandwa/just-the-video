cd0cde08dca99998e53019c271e6079f7f4e3a49
Batandwa Colani <i@batandwa.me>
2018-04-18 16:04:20 +0200

    Swtiched to CSS for page fade.

--- web/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  'use strict';
-  // will first fade out the loading animation
-  $(".preloader").fadeOut();
-  //then background color will fade out slowly
-  $("#faceoff").delay(200).fadeOut("slow");
-}());
